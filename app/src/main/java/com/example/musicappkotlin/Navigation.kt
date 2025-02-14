db4535a593e3a8a57b4163cf994c82c7232da891
Khavi18 <khavishini18@gmail.com>
2025-02-14 20:54:13 +0800

    feat: initial commite

---
+++ app/src/main/java/com/example/musicappkotlin/Navigation.kt
@@ -0,0 +1,25 @@
+package com.example.musicappkotlin
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.musicappkotlin.ui.theme.AccountView
+import com.example.musicappkotlin.ui.theme.SubscriptionView
+
+@Composable
+fun Navigation(navController: NavController, viewModel: MainViewModel, pd: PaddingValues) {
+    NavHost(navController = navController as NavHostController, startDestination = Screen.DrawerScreen.Account.route, modifier = Modifier.padding(pd)) {
+
+        composable(Screen.DrawerScreen.Account.route) {
+            AccountView()
+        }
+        composable(Screen.DrawerScreen.Subscription.route) {
+            SubscriptionView()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/musicappkotlin/ui/theme/MainView.kt
@@ -0,0 +1,119 @@
+package com.example.musicappkotlin.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.musicappkotlin.MainViewModel
+import com.example.musicappkotlin.Navigation
+import com.example.musicappkotlin.Screen
+import com.example.musicappkotlin.screensInDrawer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainView() {
+
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val scope: CoroutineScope = rememberCoroutineScope()
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val viewModel: MainViewModel = viewModel()
+
+    val dialogOpen = remember {
+        mutableStateOf(false)
+    }
+
+    val currentScreen = remember {
+        viewModel.currentScreen.value
+    }
+
+    val title = remember {
+        mutableStateOf(currentScreen.title)
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(title.value)},
+                navigationIcon = { IconButton(onClick = {
+                    //Open Drawer
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                })  {
+                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
+                }})
+        }, scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)) {
+                items(screensInDrawer) { item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if(item.dRoute == "add_account") {
+                            dialogOpen.value = true
+                        } else {
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
+        }
+    ) {
+        Navigation(controller, viewModel, it)
+
+        AccountDialog(dialogOpen)
+    }
+
+}
+
+@Composable
+fun DrawerItem(
+    selected: Boolean,
+    item: Screen.DrawerScreen,
+    onDrawerItemClicked : () -> Unit
+) {
+    val background = if (selected) Color.DarkGray else Color.White
+    Row(
+        Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 16.dp).background(background)
+            .clickable {
+                onDrawerItemClicked()
+            }) {
+        Icon(painter = painterResource(item.icon), contentDescription = item.dTitle, Modifier.padding(end = 8.dp, top = 4.dp))
+
+        Text(text = item.dTitle, style = MaterialTheme.typography.h5)
+    }
+}
+
